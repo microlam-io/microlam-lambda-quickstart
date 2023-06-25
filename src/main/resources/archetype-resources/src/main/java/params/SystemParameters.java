b62668fa16c84086e037c3ade0143519d3733a81
Frank Afriat <frank.afriat@gmail.com>
2023-06-25 15:44:50 +0300

    Use SystemParameters constructor with AwsClient

--- src/main/resources/archetype-resources/src/main/java/params/SystemParameters.java
+++ src/main/resources/archetype-resources/src/main/java/params/SystemParameters.java
@@ -2,6 +2,7 @@ package ${package}.params;
 
 import io.microlam.utils.params.AttributesProvider;
 import io.microlam.utils.params.aws.AttributesProviderPathService;
+import tech.solusoft.aws.AwsClients;
 
 public class SystemParameters {
 
@@ -9,7 +10,7 @@ public class SystemParameters {
 
 	public static AttributesProvider load() {
 		if (instance == null) {
-			instance = AttributesProviderPathService.createAtributesProvider("/${artifactId}");
+			instance = AttributesProviderPathService.createAtributesProvider("/${artifactId}", 5*60*1000l /* 5 min */, AwsClients.getSsmClient()););
 		}
 		return instance;
 	}
